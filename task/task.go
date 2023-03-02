a355712dc30463a1d1be564f67c469f88de9a659
LepikovStan <lepikov.free@yandex.ru>
2023-03-02 13:19:18 +0300

    add fn for converting task to google pubsub message and back

--- task/task.go
+++ task/task.go
@@ -1,6 +1,28 @@
 package task
 
+import (
+	"encoding/json"
+
+	"cloud.google.com/go/pubsub"
+)
+
 type Task struct {
-	ID   int    `json:"id"`
-	Text string `json:"text"`
+	ID           int    `json:"id"`
+	OriginalText string `json:"original_text"`
+}
+
+type ProvideSERPTask struct {
+	Task
+	Keyword string `json:"keyword"`
+}
+
+func TaskToGooglePubSubMessage(t any) (*pubsub.Message, error) {
+	taskBytes, err := json.Marshal(t)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pubsub.Message{
+		Data: taskBytes,
+	}, nil
 }
