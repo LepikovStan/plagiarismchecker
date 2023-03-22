d759dff315c79f36c89897b10582825e9fc74109
LepikovStan <lepikov.free@yandex.ru>
2023-03-22 13:13:08 +0100

    add page to ArticleScraperTask

--- page/page.go
+++ page/page.go
@@ -1,10 +1,10 @@
 package page
 
 type Page struct {
-	ID                 int
-	PlagiarisedPercent int
-	ReportID           int
-	Title              string
-	URL                string
-	Description        string
+	ID                 int    `json:"id"`
+	PlagiarisedPercent int    `json:"plagiarised_percent"`
+	ReportID           int    `json:"report_id"`
+	Title              string `json:"title"`
+	URL                string `json:"url"`
+	Description        string `json:"description"`
 }
