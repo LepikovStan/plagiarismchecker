ea06ce0c1e92c77599a645b0ecf8af5ed6c33cca
LepikovStan <lepikov.free@yandex.ru>
2023-04-07 11:51:33 +0200

    plagiarised sentences to page

--- page/page.go
+++ page/page.go
@@ -1,10 +1,15 @@
 package page
 
+import (
+	"github.com/lib/pq"
+)
+
 type Page struct {
-	ID                 int    `json:"id"`
-	PlagiarisedPercent int    `json:"plagiarised_percent"`
-	ReportID           int    `json:"report_id"`
-	Title              string `json:"title"`
-	URL                string `json:"url"`
-	Description        string `json:"description"`
+	ID                   int           `json:"id"`
+	PlagiarisedPercent   int           `json:"plagiarised_percent"`
+	ReportID             int           `json:"report_id"`
+	Title                string        `json:"title"`
+	URL                  string        `json:"url"`
+	Description          string        `json:"description"`
+	PlagiarisedSentences pq.Int64Array `gorm:"type:integer[]"`
 }
