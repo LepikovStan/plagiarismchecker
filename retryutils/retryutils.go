cf34d22dea4da9eae8b358bb40b265def5ab2559
LepikovStan <lepikov.free@yandex.ru>
2023-04-18 14:01:57 +0200

    add retryutils

--- retryutils/retryutils.go
+++ retryutils/retryutils.go
@@ -1,5 +1,10 @@
 package retryutils
 
+import (
+	"context"
+	"time"
+)
+
 func Retry(ctx context.Context, fn func(), delays []time.Duration) {
 	for i := 0; i < len(delays); i++ {
 		delay := delays[i]
